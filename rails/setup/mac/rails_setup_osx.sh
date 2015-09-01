37343c5b7281b75c3daa63fd23a34913266fac3f
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2015-09-01 10:18:33 +0800

    added first version of rails installation for MAC
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

---
+++ rails/setup/mac/rails_setup_osx.sh
@@ -0,0 +1,49 @@
+
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install Rbenv
+brew install rbenv ruby-build
+	# Add rbenv to bash so that it loads every time you open a terminal
+echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
+source ~/.bash_profile
+	# Install Ruby
+echo "Ruby Version (E.g: 2.2.3):"
+rbenv install 2.2.3
+rbenv global 2.2.3
+ruby -v
+
+# Install GIT
+	# Install
+echo "Go to GIT official webpage and install git manually."
+echo "Press ENTER to continue after the installation."
+read temp
+
+	# Setup Git
+echo "Your Name: "
+read username
+echo "Your Email: "
+read email
+git config --global color.ui true
+git config --global user.name $username
+git config --global user.email $email
+ssh-keygen -t rsa -C $email
+
+
+# Install Rails
+	# Install rails
+gem install rails
+
+	# Have rbenv to learn rails
+rbenv rehash
+
+# Install Postgresql
+	# Installation
+brew install postgresql
+	# To have launchd start postgresql at login:
+ln -sfv /usr/local/opt/postgresql/*plist ~/Library/LaunchAgents
+	# Then to load postgresql now:
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+
+# Done
+echo "Setup Completed. You should try generate a rails app to test out."
