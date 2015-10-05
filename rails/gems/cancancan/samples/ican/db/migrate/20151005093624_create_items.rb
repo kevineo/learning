45d72a3f699717827920a4d54a1359b1e7aa194d
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2015-10-05 17:48:30 +0800

    rails: gems: cancancan: fix migration bug for .decimal
    couldn't migrate as the column was not shown as:
    	X.decimal :price, precision: 5, scale: 2
    
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- rails/gems/cancancan/samples/ican/db/migrate/20151005093624_create_items.rb
+++ rails/gems/cancancan/samples/ican/db/migrate/20151005093624_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration
     create_table :items do |t|
       t.string :name
       t.text :description
-      t.decimal{5, 2}, :price
+      t.decimal :price, precision: 5, scale: 2
       t.belongs_to :user, index: true, foreign_key: true
 
       t.timestamps null: false
