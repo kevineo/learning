8c139944cd18260b77196685fba8cd0caaae5b2e
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2015-10-05 17:34:15 +0800

    rails: gems: cancancan: created role scaffold for iCan sample app
    Command used:
    	$ rails g scaffold role name:string description:string
    	$ rails db:migrate
    
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- rails/gems/cancancan/samples/ican/db/schema.rb
+++ rails/gems/cancancan/samples/ican/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005092602) do
+ActiveRecord::Schema.define(version: 20151005093352) do
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
