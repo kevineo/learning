0b2dbd3b22d39e069c7afa413c9d48a06c595e83
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2015-10-05 17:51:03 +0800

    rails: gems: cancancan: generated Devise User with Roles
    Commands:
    	$ rails g scaffold user name:string role:belongs_to
    	$ rails g devise User
    	$ rake db:migrate
    
    Note:
    1) Ignore the previous user generation as the role was neglected.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- rails/gems/cancancan/samples/ican/config/routes.rb
+++ rails/gems/cancancan/samples/ican/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   resources :items
   resources :roles
-  devise_for :users
   root to: 'pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
