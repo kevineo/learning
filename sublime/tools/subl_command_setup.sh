2d8c28dd013459dc2dffc4eb690b14d527dce454
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2015-10-26 10:09:21 +0800

    sublime: tools: subl_command_setup.sh: added subl alias setup script
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

---
+++ sublime/tools/subl_command_setup.sh
@@ -0,0 +1,7 @@
+subl_path="$(sudo find / -name subl)"
+if [ -z "${subl_path}" ]
+	echo "alias subl=${subl_path}" > ~/.bashrc
+	source ~/.bashrc
+else
+	echo "Couldn't find sublime binary. Have you installed the Sublime?"
+fi
