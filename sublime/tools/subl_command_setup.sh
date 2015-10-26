a313407741cbb964b00c682672bd01c1a5fa3774
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2015-10-26 10:27:47 +0800

    sublime: tools: subl_command_setup: fixed text bug
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- sublime/tools/subl_command_setup.sh
+++ sublime/tools/subl_command_setup.sh
@@ -1,7 +1,8 @@
 subl_path="$(sudo find / -name subl)"
-if [ -z "${subl_path}" ]
-	echo "alias subl=${subl_path}" > ~/.bashrc
+if ! [ -z "${subl_path}" ]
+	echo "alias subl=\"${subl_path}\"" >> ~/.bashrc
 	source ~/.bashrc
+	echo "subl command setup completed."
 else
 	echo "Couldn't find sublime binary. Have you installed the Sublime?"
 fi
