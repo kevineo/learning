8057e96ace3a3a069e2716a95c4c534a93d1d851
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2015-11-01 14:17:32 +0800

    rails: gems: actionmailer: added smtp mailer settings
    Instruction:
    http://stackoverflow.com/questions/11007061/error-when-sending-email-confirmation-using-actionmailer-on-ror
    
    https://howilearnedrails.wordpress.com/2014/02/25/setting-up-email-in-a-rails-4-app-with-action-mailer-in-development-and-sendgrid-in-production-using-heroku/comment-page-1/
    
    http://www.gotealeaf.com/blog/handling-emails-in-rails
    
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- rails/gems/action-mailer/examples/sample_app/config/environments/production.rb
+++ rails/gems/action-mailer/examples/sample_app/config/environments/production.rb
@@ -79,12 +79,16 @@ Rails.application.configure do
 
   # Confiture SMTP for Action Mailer
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: ENV['mailer_domain'] }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address:                  "smtp.gmail.com",
     port:                     587,
-    user_name:                ENV['gmail_username'],
-    password:                 ENV['gmail_password'],
-    authentication:           "plain",
+    domain:                   "gmail.com",
+    user_name:                ENV['mailer_username'],
+    password:                 ENV['mailer_password'],
+    authentication:           :plain,
     enable_starttls_auto:     true
   }
 end
