26c686c5e8b98026b14bd5b31f52fa7e37ed21ad
holloway <hollowaykeanho@gmail.com>
2015-11-25 15:46:12 +0800

    ionics: setup: setup_ubuntu_14_04.sh: added setup script for ubuntu 14.04
    Signed-off-by: holloway <hollowaykeanho@gmail.com>

---
+++ ionic/setup/setup_ubuntu_14_04.sh
@@ -0,0 +1,10 @@
+# Setup Nodejs
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Install Cordova
+sudo npm install -g cordova
+
+# Install Ionic
+sudo npm install -g ionic
