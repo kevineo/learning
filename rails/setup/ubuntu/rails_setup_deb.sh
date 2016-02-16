835440b28727b23ff1e36f570c6acd5f978a5f6a
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2016-02-16 23:19:58 +0800

    rails: setup: updated ubuntu version to have functional execution
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- rails/setup/ubuntu/rails_setup_deb.sh
+++ rails/setup/ubuntu/rails_setup_deb.sh
@@ -1,60 +1,128 @@
-echo -n "Your Name: "
-read username
-echo -n "Your Email: "
-read email
+# NOTICE
+# This script is meant for auto-run. If you want to learn, please learn BASH script. Thank you.
 
-# Install Postgresql
-sudo apt-get update -y
-sudo apt-get install -y postgresql-common postgresql-contrib
-sudo apt-get install -y postgresql libpq-dev
+function install_postgresql {
+	echo '== Install Postgresql =='
+
+	local user=$1
+
+	sudo apt-get update -y
+	sudo apt-get install -y postgresql-common postgresql-contrib
+	sudo apt-get install -y postgresql libpq-dev
+
 	# Setup user for Postgresql
-sudo -u postgres createuser $USER -s
-sudo -u postgres psql --command "\\password $USER"
+	sudo -u postgres createuser $user -s
+	sudo -u postgres psql --command "\\password $user"
 
-# Setup ssh
-ssh-keygen -t rsa -C $email << EOF
+	# Run the setup
+	service postgresql start
+	sudo update-rc.d postgresql enable
 
-
-
-EOF
-ssh-add ~/.ssh/id_rsa
-
-# Install Dependency
-sudo apt-get update -y
-sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libgmp3-dev
-
-# Install Ruby
-	# Install RVM
-sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -L https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-echo 'source ~/.rvm/scripts/rvm' >> ~/.bashrc
-
-	# Install Ruby
-echo 'Installing Ruby version with 2.2.3'
-version=2.2.3
-rvm install $version
-rvm use $version --default
-
-	# Setup Gemfile
-echo "gem: --no-ri --no-rdoc" > ~/.gemrc
-gem install bundler
+	echo '[ COMPLETED ]'
+}
 
 
-# Setup Git
-git config --global color.ui true
-git config --global user.name $username
-git config --global user.email $email
+function setup_ssh {
+	local $email=$1
+
+	echo -e "\n\n\n" | ssh-keygen -t rsa
+	ssh-add ~/.ssh/id_rsa
+}
+
+function install_dependencies {
+	sudo apt-get update -y
+	sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libgmp3-dev
+}
+
+function install_rvm {
+	sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+	gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+	curl -L https://get.rvm.io | bash -s stable
+	source ~/.rvm/scripts/rvm
+	echo 'source ~/.rvm/scripts/rvm' >> ~/.bashrc
+}
+
+function install_ruby {
+	local version=$1
+	rvm install $version
+	rvm use $version --default
+}
+
+function install_bundler {
+	echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+	gem install bundler
+}
+
+function setup_git {
+	local username=$1
+	local email=$2
+
+	git config --global color.ui true
+	git config --global user.name $username
+	git config --global user.email $email
+	git config --global push.default matching
+}
+
+function install_rails {
+	sudo apt-get update -y
+	sudo apt-get install -y nodejs
+	gem install rails
+}
 
 
-# Install Rails
-	# Install Node JS dependency
-sudo apt-get update -y
-sudo apt-get install -y nodejs
+# Welcome Printout
+echo "==================================="
+echo " Welcome to Rails Automation Tool  "
+echo " brought to you by:                "
+echo " hollowaykeanho@gmail.com          "
+echo "==================================="
+echo ""
 
-	# Install Rails
-gem install rails
+# Inquire input
+echo "-----------------------------------"
+echo "| Inquire Input                   |"
+echo "-----------------------------------"
+echo ""
+
+echo -n "Your Name For Github Signature: "
+read user_username
+echo -n "Your Email For Github Signature: "
+read user_email
+
+
+# Inquire Ruby Version to be installed
+OPTIONS="2.2.3 2.2.4"
+echo "Please select your desired ruby version option. "
+select opt in $OPTIONS; do
+	case $opt in
+		"2.2.4")
+			ruby_version=2.2.4
+			break
+			;;
+		"2.2.3")
+			ruby_version=2.2.3
+			break
+			;;
+		*)
+			echo "select option: invalid option"
+			;;
+	esac
+done
+
+install_postgresql $user_username
+
+echo "==================================================="
+echo "Inquiry completed. Press Enter to begin automation."
+echo "==================================================="
+read temp
+
+setup_ssh $user_email
+install_dependencies
+install_rvm
+install_ruby $ruby_version
+install_bundler
+setup_git $user_username $user_email
+install_rails
 
 # Done
 source ~/.bashrc
